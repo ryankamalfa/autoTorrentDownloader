0b2c8c183de14e134575db47a51f4851bd4754af
ryan kamal <social.ryankamal@gmail.com>
2022-11-22 01:01:21 +0200

    A

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ puppeteer.launch({
     // })
 
     // return;
-    let magnetURI = 'magnet:?xt=urn:btih:209c8226b299b308beaf2b9cd3fb49212dbd13ec&dn=Tears+of+Steel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Ftears-of-steel.torrent';
+    let magnetURI = 'magnet:?xt=urn:btih:15fa3f0cecc5a671a65bde79a5edbeadaa1b9555&amp;dn=Palm.Beach.2019.BDRip.x264-WaLMaRT&amp;tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&amp;tr=udp%3A%2F%2F9.rarbg.me%3A2930&amp;tr=udp%3A%2F%2F9.rarbg.to%3A2820';
     client.add(magnetURI, { path: `${__dirname}/videos` }, function (torrent) {
         torrent.on('download', function (bytes) {
             console.log('just downloaded: ' + bytes)
