5286a890e9a24735b3b353f6e04a5a6065e87f12
ryan kamal <social.ryankamal@gmail.com>
2022-11-22 13:43:21 +0200

    A

--- index.js
+++ index.js
@@ -7,43 +7,68 @@ const download = require('download');
 const WebTorrent = require('webtorrent')
 const client = new WebTorrent()
 
+// var Client = require('node-torrent');
+// var client = new Client({logLevel: 'DEBUG'});
 
-let url = 'https://rarbgaccess.org/torrent/qve4smn';
+
+let url = 'https://rarbgaccess.org/torrent/p41qe7y';
 puppeteer.launch({ 
     headless: false,
     executablePath:'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
  }).then(async browser => {
-    // const page = await browser.newPage()
-    // // await page.setViewport({ width: 800, height: 600 })
-    // await page.goto(url);
-    // let magnetURI = await page.evaluate(function(){
-    //     return document.querySelectorAll('table.lista tr td.lista a')[1].href;
-    // });
+    const page = await browser.newPage()
+    // await page.setViewport({ width: 800, height: 600 })
+    await page.goto(url);
+    let magnetURI = await page.evaluate(function(){
+        return document.querySelectorAll('table.lista tr td.lista a')[1].href;
+    });
     // let filename = magnetURI.split('&f=')[1];
-    // console.log(magnetURI);
-    // console.log('start downloading file');
+    console.log(magnetURI);
+    console.log('start downloading file');
 
 
 
 
     // download(magnetURI,`${__dirname}/torrentFiles`,{name:filename})
     // .then(() => {
-    //     console.log('Download Completed');
-    // })
+        // console.log('Download Completed');
+
+
+        // let magnetURI = `${__dirname}/torrentFiles/${filename}`;
+        client.add(magnetURI, { path: `${__dirname}/videos` }, function (torrent) {
+            torrent.on('download', function (bytes) {
+                // console.log('just downloaded: ' + bytes)
+                // console.log('total downloaded: ' + torrent.downloaded)
+                console.log('download speed: ' + torrent.downloadSpeed)
+                console.log('progress: ' + torrent.progress)
+              })
+            torrent.on('done', function () {
+              console.log('torrent download finished')
+            })
+          })
+      //   var torrent = client.addTorrent(`${__dirname}/torrentFiles/${filename}`);
+      //   torrent.on('download', function(data) {
+      //     console.log('----------');
+      //   })
+
+      //   torrent.on('downprogressload', function(data) {
+      //     console.log('----------2222');
+      //   })
+
+
+      //   torrent.on('complete', function() {
+      //     console.log('complete!');
+      //     // torrent.files.forEach(function(file) {
+      //     //     var newPath = '/new/path/' + file.path;
+      //     //     fs.rename(file.path, newPath);
+      //     //     // while still seeding need to make sure file.path points to the right place
+      //     //     file.path = newPath;
+      //     // });
+      // });
+    // });
 
     // return;
-    let magnetURI = 'magnet:?xt=urn:btih:15fa3f0cecc5a671a65bde79a5edbeadaa1b9555&amp;dn=Palm.Beach.2019.BDRip.x264-WaLMaRT&amp;tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&amp;tr=udp%3A%2F%2F9.rarbg.me%3A2930&amp;tr=udp%3A%2F%2F9.rarbg.to%3A2820';
-    client.add(magnetURI, { path: `${__dirname}/videos` }, function (torrent) {
-        torrent.on('download', function (bytes) {
-            console.log('just downloaded: ' + bytes)
-            console.log('total downloaded: ' + torrent.downloaded)
-            console.log('download speed: ' + torrent.downloadSpeed)
-            console.log('progress: ' + torrent.progress)
-          })
-        torrent.on('done', function () {
-          console.log('torrent download finished')
-        })
-      })
+    
     
     // var torrent = client.addTorrent(`${__dirname}/torrentFiles/${filename}`);
     // torrent.on('progress', function(data) {
